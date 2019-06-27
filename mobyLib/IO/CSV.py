1edcc8f1c2e35eb2c8cedf0e26027b9f6d6c93e7
mesosbrodleto <cintia@di.unipi.it>
2019-06-27 16:02:50 +0200

    code fro trajectory cut and CSV source (to be tested)

---
+++ mobyLib/IO/CSV.py
@@ -0,0 +1,196 @@
+from .trajectory import Trajectory
+from ..utils import distances
+
+from geojson import LineString
+from datetime import datetime
+
+
+
+class Trajectory(Trajectory):
+
+    def create_trajectory(self,input_data,stop_param, time_format, skip_ordering = False, store = False):
+        """
+        trajectory creation from input CSV file
+
+        input:
+            input_data: file cursor
+        """
+
+        #check points for error
+        for point in point_data:
+            self.check_mobilityData([point],self.template)
+
+        if not skip_ordering :
+            input_data = sorted(input_data, key = lambda x: (x[self.template['uid'],
+                                                            x[self.template['ts']]))
+        trajectory = []
+        user = None
+        trajectory_documents = []
+        for point in input_data:
+            uid = point[self.template['uid']]
+            lat = point[self.template['lat']]
+            lon = point[self.template['lon']]
+            ts = datetime.strftime(point[self.template['ts']],time_format)
+            # additional_keys
+            custom_features = {k:point[self.template[k]] for k in self.template if k not in ['uid','tid','lat','lon','ts']}
+
+            if user is None:
+                #first point
+                user = uid
+                data = [uid,lat,lon,ts] + [custom_features[k] for k in sorted(custom_fetures.keys())]
+                trajectory.append(data)
+            elif user != uid:
+                #new user
+
+                user = uid
+                data = [uid,lat,lon,ts] + [custom_features[k] for k in sorted(custom_fetures.keys())]
+                trajectory = [data]
+            else:
+                #iter over points, check for stops
+                prev_point = trajectory[-1]
+                distance = distances.haversine_np(prev_point[1:3],(lat,lon))
+                time_diff = ts - prev_point[3]
+                if distance > stop_param['distance'] or time > stop_param['time']:
+                    #trajectory cut
+                    document = {}
+                    document['geometry'] = Linestring([x[1:3] for x in trajectory])
+                    document['timestamps'] = [x[3] for x in trajectory]
+                    document['features'] = {'uid':user,'tid':len(trajectory_documents)}
+                    document['features'].update(custom_fetures)
+                    trajectory_documents.append(trajectory)
+
+                    #start new trajectory
+                    data = [uid,lat,lon,ts] + [custom_features[k] for k in sorted(custom_fetures.keys())]
+                    trajectory = [data]
+                else:
+                    # adding to  current trajectory
+                    data = [uid,lat,lon,ts] + [custom_features[k] for k in sorted(custom_fetures.keys())]
+                    trajectory.append(data)
+
+        return trajectory_documents
+    def check_mobilityData(self, input_data, template):
+        """
+        check if the input_data contains valid mobility data point
+
+        input:
+            input_data: list  of points
+
+        output:
+            return False if input_data has at least one invalid item
+
+        """
+
+        if not super().check_mobilityData(template):
+            #checking for required features
+            raise DataError("missing required features (uid,lat,lon,ts)")
+
+
+        for point in input_data:
+            for feature in header:
+                if point[header[feature]] is None:
+
+                    raise DataError("feature",feature," is not valid")
+        return True
+
+    def check_trajectory(self, trajectory_data):
+        """
+        check if input trajectories are valid
+        """
+
+        for trajectory in trajectory_data:
+            if 'geometry' not in trajectory:
+                raise FeatureError("missing geometry")
+            if 'timestamps' not in trajectory or len(trajectory['geometry'].coordinates) != len(timestamps):
+                raise FeatureError("missing timestamps")
+            if 'features' not in trajectory:
+                raise FeatureError("missing features for trajectory")
+
+    def load_trajectory(self, trajectory_data, input):
+        """
+        load previously saved trajectories from csv
+        format is the following;
+        uid,tid,lat,lon,ts,custom features
+
+        """
+        user = None
+        trajectory_documents = []
+        trajectory = []
+        with open(input) as f:
+
+            traj_data = [x.split(",") for x in f.readlines()]
+
+            header = traj_data[0]
+            traj_data = data[1:]
+            for data in traj_data:
+                if user is None:
+                    #first line
+                    user = data[0]
+                    tid =  data[1]
+                    trajectory.append(data)
+                else:
+                    if data[0]!=user:
+                        #new user found, cut current trajectory
+
+                        document['geometry'] = Linestring([x[1:3] for x in trajectory])
+                        document['timestamps'] = [x[3] for x in trajectory]
+                        document['features'] = {'uid':user,'tid':tid}
+                        custom_features = {header[i] : data[i] for i in range(4,len(data))}
+                        document['features'].update(custom_fetures)
+                        trajectory_documents.append(document)
+
+                        #start new traj
+                        user = data[0]
+                        tid = data[1]
+                        trajectory = [data]
+
+                    elif tid!=data[1]:
+                        document = {}
+                        document['geometry'] = Linestring([x[1:3] for x in trajectory])
+                        document['timestamps'] = [x[3] for x in trajectory]
+                        document['features'] = {'uid':user,'tid':tid}
+                        custom_features = {header[i] : data[i] for i in range(4,len(data))}
+                        document['features'].update(custom_fetures)
+                        trajectory_documents.append(document)
+                        tid=data[1]
+
+                        trajectory = [data]
+                    else:
+                        #add data to current trajectory
+                        trajectory.append(data)
+        return trajectory_documents
+
+
+    def dump_trajectory(self, trajectory_data, output):
+        """
+        store trajectory into specified output file.
+        format is the following;
+        uid,tid,lat,lon,ts,custom features
+
+        """
+
+        with open(output) as f:
+            header = "uid,tid,lat,lon,ts"
+            additional_keys = sorted([k for k in trajectory['features'] if k not in ['uid','tid']])
+            header + = ",".join(additional_keys)
+            for tn, trajectory in enumerate(trajectory_data):
+                #format csv string
+                user_id = trajectory['features']['uid']
+                traj_id = trajectory['features']['tid']
+                line = "%s,%s"%(user_id,traj_id)
+                for i,point in enumerate(trajectory['geometry']['coordinates']):
+                    ts = trajectory['timestamps'][i]
+                    line.append(",%s,%s,%s"%(point[0],point[1],ts))
+
+
+                for k in additional_keys:
+                    line.append(",%s"%trajectory['features'][k])
+                if tn == 0: #print header
+                    header += ",".join(additional_keys)
+                    print (header, file = f)
+                print (line, file=f)
+
+
+
+    def __init__(self, file_path, template):
+        self.input_file = file_path
+        self.template = template
---
+++ mobyLib/IO/trajectory.py
@@ -0,0 +1,119 @@
+
+from abc import ABC, abstractmethod
+
+class CSVError(Exception):
+   """Base class for other exceptions"""
+   pass
+
+
+class FeatureError(Error):
+   """Raised when trajectory is missing required feature"""
+   pass
+
+class DataError(Error):
+   """Raised when data is missing required feature"""
+   pass
+
+
+
+
+class Trajectory(ABC):
+    """
+    Abstract class for Trajectory document.
+
+    A trajectory is a JSON document,
+    MobyLib allows for custom defintion of trajectory document format
+
+    A possible format for Trajectory document is the following:
+
+    {
+    'geometry' : GEOJson document with trajectory points
+    'timestamps' : list of timestamps corresponding to trajectory points.
+                   Corresponding should be 1:1, one timestamp for each point
+
+   'features' :
+             {
+             'tid' : trajectory identificator
+             'uid' : user identificator
+             '<custom feature>' : <custom value>
+             }
+    }
+
+
+    """
+
+    @abstractmethod
+    def create_trajectory(self, input_data, stop_param, skip_ordering = False, store = None):
+        """
+        Function to create trajectory from input data (csv, db table, colelction etc)
+
+
+        Input:
+            input_data: pointer to input data
+            stop_param : dictionary, parameters for stop detection (space and time from point to next one)
+            time_format : string, format of datetime according to datetime library standard
+            skip_ordering: boolean, set to True if input data is already ordered by uid and times
+            to_file: function. If defined, trajectories are stored (using store())
+            instead of returning the whole list of trajectories
+
+        Output:
+            list of trajectory documents, if store is False. Else, None is returned
+
+        """
+        return
+
+    @abstractmethod
+    def dump_trajectory(self, trajectory_data, output):
+        """
+        Function to store trajectories.
+
+        Input:
+            trajectory_data: pointer to trajectory documents
+            output: function to store data (writing to file for CSV, writing table for db, etc)
+        """
+
+    @abstractmethod
+    def load_trajectory(self, trajectory_data, output):
+        """
+        Function to store trajectories.
+
+        Input:
+            trajectory_data: pointer to trajectory documents
+            output: function to store data (writing to file for CSV, writing table for db, etc)
+        """
+
+    @abstractmethod
+    def check_trajectory(self, trajectory_data):
+        """
+        Function to check if input trajectory documents are compliant with the format.
+        It is used to design the structure of trajectory document
+
+        Input:
+            trajectory_data: pointer to trajectory documents
+        """
+
+
+    @abstractmethod
+    def check_mobilityData(self, input_data, template):
+        """
+        Check if input data is compliant with the mobility data required to create a trajectory
+
+        Input:
+            input_data: pointer to input data
+            template: dictionary to specify semantics of input_data.
+                    e.g. for CSV it contains columns indexes {'id':0,'lat':1, ...}
+
+        Note: mandatory field for mobility data template are:
+
+        uid: user identificator
+        lat: latitude coordinate
+        lon: longitude coordinate
+        ts: timestamp coordinate
+
+        Output: boolean
+        """
+
+        for feature in ['uid','lat','lon','ts']:
+            if feature not in template:
+                return False
+        return True
